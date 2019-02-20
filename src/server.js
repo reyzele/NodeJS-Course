77abdc65b65262018330bad5cffc12d19f2e1f34
Vadim Veksler <reyzele91@gmail.com>
2019-02-20 09:53:03 +0200

    DZ2 part 1 done

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const port = 3000;
+const timerInterval = 1000;
+const timerStop = 5000;
+const server = http.createServer((req, res) => {
+  var timer = setInterval(() => {
+    if (req.url === '/') {
+      console.log(getTime());
+      console.log(process.memoryUsage().heapUsed);
+    }
+  }, timerInterval);
+
+  setTimeout(() => {
+    res.end(getTime());
+    clearInterval(timer);
+  }, timerStop);
+});
+
+function getTime () {
+  return new Date(Date.now()).toString();
+}
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
