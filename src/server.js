f54db9c2b5c4ce8a0d471f6a2a64a4de35d72cd5
Vadim Veksler <reyzele91@gmail.com>
2019-02-24 19:14:29 +0200

    update server.js

--- src/server.js
+++ src/server.js
@@ -5,19 +5,20 @@ const port = process.env.PORT;
 const timerInterval = process.env.INTERVAL_TIME;
 const timerStop = process.env.STOP_INTERVAL_TIME;
 const server = http.createServer((req, res) => {
-  const timeEnd = getTime() + +timerStop;
-  let timeNow;
+  if (req.method === "GET") {
+    const timeEnd = getTime() + +timerStop;
+    let timeNow;
+    var timer = setInterval(() => {
+      timeNow = getTime();
 
-  var timer = setInterval(() => {
-    timeNow = getTime();
-
-    if (req.url === "/" && timeNow <= timeEnd) {
-      console.log(timeNow);
-    } else {
-      res.end(new Date(timeNow).toString());
-      clearInterval(timer);
-    }
-  }, timerInterval);
+      if (req.url === "/" && timeNow <= timeEnd) {
+        console.log(timeNow);
+      } else {
+        res.end(new Date(timeNow).toString());
+        clearInterval(timer);
+      }
+    }, timerInterval);
+  }
 });
 
 function getTime() {
